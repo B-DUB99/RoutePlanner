5800496cf5eae2132efd2309d949ccea5f46197c
BDUB <79788719+gaming-B-DUB@users.noreply.github.com>
2023-01-26 09:28:40 -0500

    some OSMPythonTools testing
    https://pypi.org/project/OSMPythonTools/

---
+++ testing/bw.py
@@ -0,0 +1,22 @@
+import OSMPythonTools
+from OSMPythonTools.api import Api
+
+
+from OSMPythonTools.nominatim import Nominatim
+nominatim = Nominatim()
+areaId = nominatim.query('Vienna, Austria').areaId()
+
+
+from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
+overpass = Overpass()
+query = overpassQueryBuilder(area=areaId, elementType=['way', 'relation'], selector='"natural"="water"', includeGeometry=True)
+result = overpass.query(query)
+
+
+firstElement = result.elements()[0]
+firstElement.geometry()
+# {"coordinates": [[[16.498671, 48.27628], [16.4991, 48.276345], ... ]], "type": "Polygon"}
+
+
+
+
