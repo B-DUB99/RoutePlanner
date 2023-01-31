2b15f5e3b19eede8bd3a93c45674e4ba228c9d94
BDUB <79788719+gaming-B-DUB@users.noreply.github.com>
2023-01-31 10:09:26 -0500

    bw - 31.01.2023

--- testing/bw.py
+++ testing/bw.py
@@ -1,23 +1,70 @@
-import OSMPythonTools
-from OSMPythonTools.api import Api
-
-
-from OSMPythonTools.nominatim import Nominatim
-nominatim = Nominatim()
-areaId = nominatim.query('Vienna, Austria').areaId()
-
-
-from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
-overpass = Overpass()
-query = overpassQueryBuilder(area=areaId, elementType=['way', 'relation'], selector='"natural"="water"', includeGeometry=True)
-result = overpass.query(query)
-
-
-firstElement = result.elements()[0]
-firstElement.geometry()
-# {"coordinates": [[[16.498671, 48.27628], [16.4991, 48.276345], ... ]], "type": "Polygon"}
+import plotly.express as px
+import pandas as pd
 
 
 
+def plotly_bw_car_parking():
+    print("getting Data...")
+    df = px.data.carshare()
+    print(df.head(10))
 
 
+    fig = px.scatter_mapbox(df,
+                            lat="centroid_lat",
+                            lon="centroid_lon",
+                            color="peak_hour",
+                            size="car_hours",
+                            title="Carshare Data",
+                            width=1200, height=800, zoom=10)
+
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
+
+
+
+
+
+def OSM_Data_Test():
+    from OSMPythonTools.nominatim import Nominatim
+    from OSMPythonTools.overpass import overpassQueryBuilder
+    from OSMPythonTools.overpass import Overpass
+
+
+    # nominatim = Nominatim()
+    # nyc = nominatim.query('NYC')
+
+    # query = overpassQueryBuilder(area=nyc, elementType='node', selector='"highway"="bus_stop"', out='body')
+
+    # query = overpassQueryBuilder(area=nyc.areaId(), elementType='node', selector='"highway"="bus_stop"', out='body')
+    # query = overpassQueryBuilder(area='relation/175905', elementType='node', selector='"highway"="bus_stop"', out='body')
+    # query = overpassQueryBuilder(area='relation 175905', elementType='node', selector='"highway"="bus_stop"', out='body')
+
+    query = overpassQueryBuilder(bbox=[48.1, 16.3, 48.3, 16.5], elementType='node', selector='"highway"="bus_stop"', out='body', includeGeometry=True)
+
+
+
+    overpass = Overpass()
+    busStops = overpass.query(query)
+
+    # print(busStops.nodes()[0])
+    print(busStops.nodes()[0].tags())
+
+
+
+
+    # print(busStops.elements())
+
+
+
+
+
+
+def main():
+    # plotly_bw_car_parking()
+    OSM_Data_Test()
+
+
+
+if __name__ == "__main__":
+    main()
