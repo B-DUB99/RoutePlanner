56aa3b2aff8d760b7a7a2c257ac589d2bb1ef46e
BDUB <79788719+gaming-B-DUB@users.noreply.github.com>
2023-02-21 10:14:44 -0500

    help austin with git
    a ittle base start for austin :)

---
+++ testing/austin/views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request, session, redirect, url_for
+import json
+
+views = Blueprint(__name__, "")
+locs = []
+
+
+@views.route("/", methods=["GET","POST"])
+def test():
+    # will be post when you click on the map to put a marker
+    if request.method == "POST":
+        output = request.get_json()
+        coords = json.loads(output)
+        locs.append(coords)
+        print(locs)
+    return render_template("testingwithmenu.html")
