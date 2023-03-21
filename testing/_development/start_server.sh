1044ff07747ff1a9a0382c81d78d9c3edbedb9ac
BDUB <79788719+B-DUB99@users.noreply.github.com>
2023-03-21 09:17:29 -0400

    same as on _for_server_
    again,.. Don't use the Update Button, It WILL Break the Universe :)

--- testing/_development/start_server.sh
+++ testing/_development/start_server.sh
@@ -6,5 +6,7 @@ SCREEN_NAME="Senior_Design"
 # Command to start the Python script
 COMMAND="python3 main.py"
 
+sleep 20
+
 # Create a new screen session with the given name, and start the Python script inside it
 screen -dmS $SCREEN_NAME $COMMAND
--- testing/_development/update.sh
+++ testing/_development/update.sh
@@ -2,17 +2,23 @@
 
 # Name of the screen to check for
 SCREEN_NAME="Senior_Design"
+SCREEN_PID=$(screen -ls | grep "$SCREEN_NAME" | awk '{print $1}' | cut -d '.' -f1)
 
 # Check if the screen is running
 if screen -list | grep -q "\.$SCREEN_NAME"; then
   # If the screen is running, close it
-  screen -S $SCREEN_NAME -X quit
   
+  echo "PID of $SCREEN_NAME is: $SCREEN_PID"
+
   # Git pull
   git pull
-  
+
   # Call a different bash script
   ./start_server.sh
+
+  screen -S $SCREEN_PID -X quit
+  
+
 else
   echo "Screen is not running"
 fi
