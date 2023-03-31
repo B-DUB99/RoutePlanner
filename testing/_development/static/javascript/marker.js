3216c21560b5f3aed5223e68a59f7198362c0079
Christian Fuentes <christianfuentes@Christians-MacBook-Pro-2.local>
2023-03-31 15:57:04 -0400

    nevermind

--- testing/_development/static/javascript/marker.js
+++ testing/_development/static/javascript/marker.js
@@ -31,12 +31,10 @@ function createMarker(event) {
 function deleteAllMarkers() {
     let icons = document.getElementsByClassName("leaflet-marker-icon");
     let shadows = document.getElementsByClassName("leaflet-marker-shadow");
-    let line = document.getElementsByClassName("leaflet-overlay-pane");
     // delete markings from map
     while (icons.length > 0) {
         icons[0].remove();
         shadows[0].remove();
-        line[0].remove();
     }
     markers.clear();
 }
