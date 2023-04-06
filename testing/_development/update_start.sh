b7345c3f60a2876a205b3065d9c1449db1934801
B-DUB99 <gaming.bdub@famwilken.de>
2023-04-06 18:37:19 +0200

    update_start.sh is not crashing anymore

--- testing/_development/update_start.sh
+++ testing/_development/update_start.sh
@@ -6,29 +6,20 @@ echo "start:$DAT - $0 " >> /media/bdub/sdb1/Senior_Design/update.log
 SCREEN_NAME="Senior_Design"
 SCREENNAME="SeniorDesign"
 SCREEN_PID=$(screen -ls | grep "$SCREEN_NAME" | awk '{print $1}' | cut -d '.' -f1)
-SCREENPID=$(screen -ls | grep "$SCREENNAME" | awk '{print $1}' | cut -d '.' -f1)
 
-
-echo "git pull"
+echo "git pull - start"
 git pull
-echo "git pull done"
+echo "git pull - done"
 
 
-echo "starting server now"
-if screen -list | grep -q "\.$SCREEN_NAME"; then
+echo "checking for server now"
+if screen -list | grep -q "\.$SCREEN_NAME";
+then
   echo "PID of $SCREEN_NAME is: $SCREEN_PID"
 
-  screen -S "SeniorDesign" -dm python3 main.py &
-  screen -S $SCREEN_PID -X quit
-fi
-
-
-if screen -list | grep -q "\.$SCREENNAME"; then
-  echo "PID of $SCREENNAME is: $SCREENPID"
-  
   screen -S "Senior_Design" -dm python3 main.py &
-  screen -S $SCREENPID -X quit
+  screen -S $SCREEN_PID -X quit
+else
+  echo "Server offline, starting"
+  screen -S "Senior_Design" -dm python3 main.py &
 fi
-
-echo "Server offline, starting"
-screen -S "Senior_Design" -dm python3 main.py &
