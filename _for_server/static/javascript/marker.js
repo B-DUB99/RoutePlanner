0ffd8f003bee1e5f4250b97d50de94c01b20a4ff
BDUB <79788719+B-DUB99@users.noreply.github.com>
2023-04-22 12:59:22 -0400

    push 22.04.2023

---
+++ _for_server/static/javascript/marker.js
@@ -0,0 +1,56 @@
+var markers = new Map();
+var lines = []
+
+function createMarker(event) {
+    if (markers.size < 2) {
+        let marker = L.marker(event.latlng, {
+            draggable: true
+        });
+        // if statement to define the id 
+        var id = (markers.get(1) == undefined) ? 1 : 2;
+        marker.id = id;
+        markers.set(id, marker._latlng);
+
+        // add marker onto layer
+        marker.on("click", deleteMarker);
+        marker.on("dragend", newCoords);
+        marker.addTo(map);
+    }
+    if (markers.size == 2 && lines.length == 0){
+        var line = new L.Polyline([markers.get(1), markers.get(2)], {
+            color: 'red',
+            weight: 3,
+            opacity: 0.5,
+            smoothFactor: 1
+        });
+        lines[0] = line
+        line.addTo(map);
+    }
+}
+
+function deleteAllMarkers() {
+    let icons = document.getElementsByClassName("leaflet-marker-icon");
+    let shadows = document.getElementsByClassName("leaflet-marker-shadow");
+    while (icons.length > 0) {
+        icons[0].remove();
+        shadows[0].remove();
+    }
+    markers.clear();
+}
+
+function deleteMarker() {
+    if(markers.size == 2 || (markers.size < 2 && lines.length > 0)){
+        map.removeLayer(lines[0])
+        lines.pop()
+    }
+    map.removeLayer(this);
+    markers.delete(this.id);
+}
+
+function newCoords() {
+    markers.set(this.id, this._latlng);
+}
+
+
+map.addEventListener("click", createMarker);
+document.getElementById("reset").addEventListener("click", deleteAllMarkers);
---
+++ _for_server/views.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template, request, session, redirect, url_for
+import json
+import subprocess
+
+views = Blueprint(__name__, "")
+locs = []
+
+
+@views.route("/", methods=["GET", "POST"])
+def test():
+    # will be post when you click on the map to put a marker
+    if request.method == "POST":
+        output = request.get_json()
+        coords = json.loads(output)
+        locs.append(coords)
+        print(locs)
+    return render_template("testingwithmenu.html")
+
+@views.route("/update/")
+def update():
+    print("updating...")
+    # run bash script to pull the latest changes from the repo and restart the server (will be done automatically)
+    # subprocess.run(["./update.sh"])
+
+    return redirect(url_for("views.test"))
