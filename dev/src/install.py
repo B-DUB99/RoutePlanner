32abe641d8e267b7cccf8a5ee1a36fe027371b7e
BDUB <79788719+B-DUB99@users.noreply.github.com>
2023-05-30 18:29:14 +0200

    db now with sqllite instead of sql Server,
    The reason: sqllite is Python In-Build functuallity and might be simpler in the long run for everyone to use.

---
+++ dev/src/install.py
@@ -0,0 +1,21 @@
+# pip install all the packages needed for the website to run
+import os
+import sys
+import subprocess
+
+
+def install(package):
+	subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+	print('Installed ' + package + ' successfully! \n\n\n')
+
+
+install("numpy")
+install("flask")
+install("python-dotenv")
+install("pandas")
+install("OSMPythonTools")
+install("pykml")
+install("geopy")
+install("geopandas")
+install("folium")
+
