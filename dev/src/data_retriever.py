15cf8e17dbf7217d88eb6067ded148a2048481fe
BDUB <79788719+B-DUB99@users.noreply.github.com>
2023-09-06 16:19:46 -0400

    Changed data_retriever
    created function to connect to the DB and creation of Cursor ( if needed )

--- dev/src/data_retriever.py
+++ dev/src/data_retriever.py
@@ -1,35 +1,39 @@
+# Python modules
 import sqlite3
+
 
 class data_retriever:
 
-    connection
+	def __init__(self):
+		self.connection = None
+		self.cursor = None
 
-    def __init__(self):
-        try:
-            connection = sqlite3.connect('../db/routeplanning.db')
-            print('Successful connection')
-            return 0
-        except Exception as e:
-            print(f'Error: {e}')
-            return -1
+	# use to connect to the database and create a cursor object
+	def connect(self):
+		try:
+			self.connection = sqlite3.connect('../db/routeplanning.db')
+			self.cursor = self.connection.cursor()
+			print('Successful connection, cursor created\n')
+			return 0
 
-    def get_closest_node(self, user_marker):
-        return
+		except Exception as e:
+			print(f'Error: {e}')
+			return -1
 
-    def get_exit_nodes(self, way_id):
-        return
+	def get_closest_node(self, user_marker):
+		return
 
-    def get_nodes(self, way_id):
-        return
-    
-    def get_way(self, node_id):
-        return
+	def get_exit_nodes(self, way_id):
+		return
 
-    def get_way_info(self, way_id):
-        return
+	def get_nodes(self, way_id):
+		return
 
-    def get_node_info(self, node_id):
-        return
+	def get_way(self, node_id):
+		return
 
+	def get_way_info(self, way_id):
+		return
 
-
+	def get_node_info(self, node_id):
+		return
