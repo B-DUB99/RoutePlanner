5fa768c1ef77bc43ea77e83de875e9fe3719a7f6
MattAP90 <Matt1333985@gmail.com>
2023-09-11 19:18:01 -0400

    Added methods to get nodes, ways, and get the amenities data from the database

--- dev/src/data_retriever.py
+++ dev/src/data_retriever.py
@@ -3,44 +3,81 @@ import sqlite3
 
 
 class data_retriever:
+    
+    def __init__(self):
+        self.connection = None
+        self.cursor = None
 
-	def __init__(self):
-		self.connection = None
-		self.cursor = None
-
-	# use to connect to the database and create a cursor object
-	def connect(self):
-		try:
-			self.connection = sqlite3.connect('../db/routeplanning.db')
-			self.cursor = self.connection.cursor()
-			print('Successful connection, cursor created\n')
-			return 0
-
-		except Exception as e:
-			print(f'Error: {e}')
-			return -1
-
+    # use to connect to the database and create a cursor object
+    def connect(self):
+        try:
+            self.connection = sqlite3.connect('../db/routeplanning.db')
+            self.cursor = self.connection.cursor()
+            print('Successful connection, cursor created\n')
+        except Exception as e:
+            print(f'Error: {e}')
+    
     def close(self):
         try:
-            connection.close()
+            self.connection.close()
         except:
             print("Close Failed")
-            return -1
 
-	def get_closest_node(self, user_marker):
-		return
+    def get_amenities(self, amen_type):
+        self.cursor.execute("SELECT * FROM amenities a1, amenity_types a2 WHERE"
+                            + f" a1.type = a2.rowid AND a2.name = '{amen_type}'")
+        
+        amens = self.cursor.fetchall()
+        amens_dict = []
+        
+        for a in amens:
+            temp_dict = [{'name': a[1],
+                          'desc': a[2],
+                          'lat': a[4],
+                          'lon': a[5],
+                          'pic_loc': a[6]}]
+            amens_dict.append(temp_dict)
 
-	def get_exit_nodes(self, way_id):
-		return
+        return amens_dict
 
-	def get_nodes(self, way_id):
-		return
+    def get_closest_node(self, user_marker, transport):
 
-	def get_way(self, node_id):
-		return
+        return
 
-	def get_way_info(self, way_id):
-		return
+    def get_exit_nodes(self, way_id):
+        return
 
-	def get_node_info(self, node_id):
-		return
+    # Returns all nodes attached to provided way_id
+    def get_nodes(self, way_id):
+        self.cursor.execute("SELECT DISTINCT node_id FROM nodes n, (SELECT "
+                            + "node_id_from, node_id_to FROM links l, ways w"
+                            + f" WHERE w.way_id = l.way_id AND l.way_id = {way_id}"
+                            + ") a WHERE n.node_id = a.node_id_from OR n.node_id "
+                            + "= a.node_id_to")
+
+        temp = self.cursor.fetchall()
+        nodes = []
+        for t in temp:
+            nodes.append(t[0])
+
+        return nodes
+
+    # Returns all ways attached to provided node_id
+    def get_way(self, node_id):
+        self.cursor.execute("SELECT DISTINCT way_id FROM links WHERE "
+                            + f"node_id_from = {node_id} OR node_id_to "
+                            + f"= {node_id}")
+
+        temp = self.cursor.fetchall()
+        ways = []
+        for t in temp:
+            ways.append(t[0])
+
+        return ways
+
+    def get_way_info(self, way_id):
+
+        return
+
+    def get_node_info(self, node_id):
+        return
