0dc70d03e4e682fa49db2e2b5d0b6d7bbe6aa9c4
BDUB <79788719+B-DUB99@users.noreply.github.com>
2023-09-27 14:26:45 -0400

    Create DB_update.py

---
+++ dev/DB_Management/DB_update.py
@@ -0,0 +1,32 @@
+# import itertools
+import sqlite3
+from collections import deque
+from geopy import distance
+from pykml import parser
+from OSMPythonTools.overpass import Overpass
+from OSMPythonTools.overpass import overpassQueryBuilder
+
+def main():
+	print("Updating database...")
+	# try to connect to the database file if it exists, otherwise create a new one
+	try:
+		connection = sqlite3.connect('../db/routeplanning.db')
+		print("Opened database successfully")
+	except:
+		print("Error connecting to database")
+		return
+
+	# create a cursor object to execute SQL commands
+	cursor = connection.cursor()
+
+	
+
+	# commit and close
+	connection.commit()
+	connection.close()
+
+
+
+
+if __name__ == "__main__":
+	main()
