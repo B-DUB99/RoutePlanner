b1d11b9315f41ca44b33cb716d3279fa04aa0aa5
BDUB <79788719+B-DUB99@users.noreply.github.com>
2023-09-28 18:50:36 -0400

    update DB when Update bash
    DB update file should be getting called and run, when we call The Manual Update Bash File

--- dev/update_start.sh
+++ dev/update_start.sh
@@ -5,11 +5,15 @@ echo "start:$DAT - $0 " >> /media/bdub/sdb1/Senior_Design/update.log
 
 SCREEN_NAME="Senior_Design"
 SCREEN_PID=$(screen -ls | grep "$SCREEN_NAME" | awk '{print $1}' | cut -d '.' -f1)
+PYTHON_PATH="/DB_Management/DB_update.py"
 
 echo "git pull - start"
 git pull
 echo "git pull - done"
 
+echo "Upgrading the DB - start: $PYTHON_PATH"
+python3 "$PYTHON_PATH"
+echo "Upgrading the DB - done"
 
 echo "checking for server now"
 if screen -list | grep -q "\.$SCREEN_NAME";
