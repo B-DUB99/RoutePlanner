c7b392e344417820f5a4d040a92c708de22beabe
BDUB <79788719+B-DUB99@users.noreply.github.com>
2023-10-03 16:12:08 -0400

    Update views.py

--- dev/src/views.py
+++ dev/src/views.py
@@ -13,7 +13,7 @@ temp_points = [
         [42.30918068099292, -85.6549072265625],
         [42.26790919743789, -85.65319061279297],
         [42.291532494305976, -85.58795928955078]
-    ]
+    
 
 
 @views.route("/", methods=["GET", "POST"])
@@ -24,12 +24,6 @@ def test():
         coords = json.loads(output)
         locs.append(coords)
         print(locs)
-    temp_points = [
-        [42.29127852746485, -85.5919075012207],
-        [42.30918068099292, -85.6549072265625],
-        [42.26790919743789, -85.65319061279297],
-        [42.291532494305976, -85.58795928955078]
-    ]
     return render_template("routeplanner.html", temp_points=temp_points, length=len(temp_points))
 
 @views.route('/<string:markerInfo>', methods=['POST'])
