072012c7b47a6fb0e38bbe545106a5f70dd73771
BDUB <79788719+B-DUB99@users.noreply.github.com>
2023-11-27 19:03:18 -0500

    FIX: "module not found error" for test.py

---
+++ src/scripts/test.py
@@ -0,0 +1,45 @@
+# Imports
+
+
+class test:
+	def __init__(self):
+		self.test_database()
+		self.test_website()
+		self.test_pathfinder()
+		self.test_dataretreiver()
+		self.test_views()
+		self.helper_timing()
+		self.helper_logging()
+		self.helper_error()
+		self.results()
+
+
+
+	# Tests
+	def test_database(self):
+		raise NotImplementedError
+
+	def test_website(self):
+		raise NotImplementedError
+
+	def test_pathfinder(self):
+		raise NotImplementedError
+
+	def test_dataretreiver(self):
+		raise NotImplementedError
+
+	def test_views(self):
+		raise NotImplementedError
+
+	def helper_timing(self):
+		raise NotImplementedError
+
+	def helper_logging(self):
+		raise NotImplementedError
+
+	def helper_error(self):
+		raise NotImplementedError
+
+	def results(self):
+		raise NotImplementedError
+
