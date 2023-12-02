2812f78d6ec654bafe7a5e80ce0bb5590b1aa492
MattAP90 <Matt1333985@gmail.com>
2023-12-01 20:50:41 -0500

    changed color of markers to match a start and end

--- src/static/javascript/marker.js
+++ src/static/javascript/marker.js
@@ -5,13 +5,43 @@ var layers = [];
 let dest;
 let pathArray;
 
+const redIcon = new L.Icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+	shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+const greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
 function createMarker(event) {
 	if (event.latlng.lat >= 42.157 && event.latlng.lat <= 42.369062 && event.latlng.lng >= -85.6995747 && event.latlng.lng <= -85.531 && markers.size != 2) {
         // used to place marker on top of map
         if (markers.size < 2) {
-            let marker = L.marker(event.latlng, {
-                draggable: true, 
-            });
+			let marker;
+			if (markers.size == 0){
+                marker = L.marker(event.latlng, {
+               	    draggable: true,
+                    icon: greenIcon,
+                    title: 'Start'
+           	    });
+			}else if(markers.size == 1){
+                marker = L.marker(event.latlng, {
+                    draggable: true,
+                    icon: redIcon,
+                    title: 'Destination'
+                });
+            }
+
             // if statement to define the id 
             var id = (markers.get(1) == undefined) ? 1 : 2;
             marker.id = id;
